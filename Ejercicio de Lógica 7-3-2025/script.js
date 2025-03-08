d5a59d61e120bacf89bf4892ec21713d04cf6d6b
DanielaContrerasF <danycf98@gmail.com>
2025-03-07 20:10:24 -0600

    Add files via upload

---
+++ Ejercicio de Lógica 7-3-2025/script.js
@@ -0,0 +1,27 @@
+//Práctica de comparación de números
+
+
+//solicitar al usuario 3 números por prompt y guardarlos en sus respectivas variables.
+//Solicitando el primer número
+let numero1 = prompt("Ingrese el primer número"); //Deja que numero 1 sea un prompt donde solicito ingresar un número
+numero1 = Number(numero1); //numero 1 va a guardar el valor que arrojo el usuario
+
+let numero2 = prompt("Ingrese el segundo número");
+numero2 = Number(numero2);
+
+let numero3 = prompt("Ingrese el tercer número");
+numero3 = Number(numero3);
+
+console.log(numero1); //Imprimiendo el valor del numero 1
+console.log(numero2); //Imprimiendo el valor del numero 2
+console.log(numero3); //Imprimiendo el valor del numero 3
+
+//Analizar los numeros e identificar el mayor, el den centro y menor
+let numeros = [numero1, numero2, numero3]; //Array para gaurdar en un mismo lugar los valores de los 3 numeros
+
+//Ordenando los números
+numeros.sort(function(a,b) { //numeros sort() --> metodo para odenar el arreglo (sort()). Function (a,b) es la que compara los elementos (a,b)
+    return a-b; //Aquí se determina como se ordenan los elementos (b-a de mayor a menor)
+});
+
+console.log(numeros); //imprimiendo el resultado
